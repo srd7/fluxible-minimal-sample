2a22bdb917d76e54518a71f1e6bfc99a0e824dcb
srd7 <kuroneko0011@gmail.com>
2015-09-12 17:51:01 +0900

    change page title after page transition

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,10 +1,20 @@
 import React from "react";
-
 import { handleHistory } from "fluxible-router";
-
+import { connectToStores } from "fluxible-addons-react";
+import ApplicationStore from "src/stores/ApplicationStore";
 import Header from "src/components/Header";
 
 class Application extends React.Component {
+  componentDidUpdate(prevProps) {
+    // 下で Store から pageTitle を取得できるようにしている
+    const prevPageTitle = prevProps.pageTitle;
+    const newPageTitle  = this.props.pageTitle;
+
+    // タイトルを再設定
+    if (prevPageTitle !== newPageTitle) {
+      document.title = newPageTitle;
+    }
+  }
   render() {
     // ルーティングからコンテンツ本体を取得
     const { currentRoute } = this.props;
@@ -21,5 +31,12 @@ class Application extends React.Component {
   }
 }
 
-// currentRoute を得るのに必要
-export default handleHistory(Application);
+// currentRoute を得るのに handleHistory が必要
+// また、タイトル取得のために ApplicationStore に接続する
+export default handleHistory(connectToStores(Application, [ApplicationStore], (context) => {
+  const applicationStore = context.getStore(ApplicationStore);
+  // これで props.pageTitle が格納される
+  return {
+    pageTitle: applicationStore.getPageTitle()
+  };
+}));
